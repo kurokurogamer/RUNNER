49e63353a693f1c6093f5a0e253e3d3f0dd712d7
a <d>
2020-10-20 09:47:23 +0900

    タイトル画面「

---
+++ RUNNER/Assets/Scripts/TitleScenes.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScenes : MonoBehaviour
+{
+
+    private void Update()
+    {
+        OnClickStartButton();
+    }
+
+    private void OnClickStartButton()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("GameScene1");
+        }
+    }
+}
