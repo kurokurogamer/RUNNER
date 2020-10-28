ef959367569b16cc24a42deffbc6d31d4f6bdd65
Shiraeda <ASOPC>
2020-10-28 10:01:00 +0900

    AudioManagerの作成

--- RUNNER/Assets/Scripts/Audio/AudioManager.cs
+++ RUNNER/Assets/Scripts/Audio/AudioManager.cs
@@ -5,47 +5,72 @@ using UnityEngine.Audio;
 
 public class AudioManager : MonoBehaviour
 {
-    private AudioManager _instans = null;
+    public static AudioManager instans = null;
 
     private AudioSource _source;
-    private AudioClip _clip = null;
 
-    public AudioManager Instans
+	private void Awake()
 	{
-		get
+		if(instans == null)
 		{
-            if(_instans != null)
-			{
-                _instans = this;
-			}
-            return _instans;
+            instans = this;
+            DontDestroyOnLoad(this.gameObject);
+		}
+		else
+		{
+            Destroy(this.gameObject);
 		}
 	}
-    // Start is called before the first frame update
-    void Start()
+
+	// Start is called before the first frame update
+	void Start()
     {
         _source = GetComponent<AudioSource>();
     }
 
     public void PlaySE()
 	{
-        _source.PlayOneShot(_clip);
+        if (_source.clip == null)
+        {
+            return;
+        }
+        _source.PlayOneShot(_source.clip);
 	}
 
     public void PlaySE(AudioClip clip)
 	{
-        _source.PlayOneShot(_clip);
+        _source.PlayOneShot(clip);
     }
 
     public void PlayBGM()
     {
-        _source.PlayOneShot(_clip);
+        if (_source.clip == null)
+        {
+            return;
+        }
+        StopAllCoroutines();
+        _source.Stop();
+        StartCoroutine(BGM(_source.clip));
     }
 
     public void PlayBGM(AudioClip clip)
     {
-        _source.PlayOneShot(_clip);
+        StopAllCoroutines();
+        _source.Stop();
+        StartCoroutine(BGM(clip));
     }
+
+    private IEnumerator BGM(AudioClip clip)
+	{
+        while(true)
+		{
+            if(_source.isPlaying == false)
+			{
+                _source.PlayOneShot(_source.clip);
+            }
+        }
+	}
+
     // Update is called once per frame
     void Update()
     {
